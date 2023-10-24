bd046f45cd9db9cfc05503960c1e9d1c04fc4ed4
hsergiu <>
2023-10-24 22:11:41 +0300

    move main to root

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import App from './components/App.vue'
+import App from './src/components/App.vue'
 
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-import ItemView from './components/ItemView.vue'
-import DetailsView from './components/DetailsView.vue'
+import ItemView from './src/components/ItemView.vue'
+import DetailsView from './src/components/DetailsView.vue'
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'build.js',
-      },
-    },
-  },
   plugins: [Vue()],
 });
